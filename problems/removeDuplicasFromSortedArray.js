abaa0ff403ac89744fcf58c697f1ac17799ab353
Joao Cardoso <joao.cardoso@nn.nl>
2024-06-24 23:43:41 +0200

    Adding removeDuplicatesFromSortedArray

---
+++ problems/removeDuplicasFromSortedArray.js
@@ -0,0 +1,33 @@
+/**
+ * Given an integer array nums sorted in non-decreasing order,
+ * remove the duplicates in-place such that each unique element appears only once.
+ * The relative order of the elements should be kept the same. 
+ * Then return the number of unique elements in nums.
+ * Consider the number of unique elements of nums to be k,
+ * to get accepted, you need to do the following things:
+ * Change the array nums such that the first k elements 
+ * of nums contain the unique elements in the order they were present in nums initially.
+ * The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ */
+
+/**
+ * @param {array} nums
+ * @return {array}
+ */
+
+const removeDuplicatesFromSortedArray = (nums) => {
+    if (!Array.isArray(nums)) {
+        return
+    }
+
+    const uniqueArray = [...new Set(nums)]
+
+    for (let i = 0; i < uniqueArray.length; i++) {
+        nums[i] = uniqueArray[i]
+    }
+
+    return uniqueArray.length
+}
+
+module.exports = removeDuplicatesFromSortedArray
---
+++ problems/removeDuplicasFromSortedArray.test.js
@@ -0,0 +1,27 @@
+const removeDuplicatesFromSortedArray = require("./removeDuplicasFromSortedArray")
+
+describe('removeDuplicatesFromSortedArray', () => {
+    test('should return undefined for invalid input', () => {
+        const invalidResponse = undefined
+        expect(removeDuplicatesFromSortedArray()).toBe(invalidResponse)
+        expect(removeDuplicatesFromSortedArray({})).toBe(invalidResponse)
+        expect(removeDuplicatesFromSortedArray(null)).toBe(invalidResponse)
+        expect(removeDuplicatesFromSortedArray('array')).toBe(invalidResponse)
+        expect(removeDuplicatesFromSortedArray(100000)).toBe(invalidResponse)
+    })
+
+    test('should return input length when input has no duplicates', () => {
+        expect(removeDuplicatesFromSortedArray([])).toEqual(0)
+        expect(removeDuplicatesFromSortedArray([1])).toEqual(1)
+        expect(removeDuplicatesFromSortedArray([1,2])).toEqual(2)
+        expect(removeDuplicatesFromSortedArray([1,2,3])).toEqual(3)
+    })
+
+    test('should return same array when input has no duplicates', () => {
+        let array = [1,1,2,2,3,3]
+        expect(removeDuplicatesFromSortedArray(array)).toEqual(3)
+        expect(array[0]).toBe(1)
+        expect(array[1]).toBe(2)
+        expect(array[2]).toBe(3)
+    })
+})
