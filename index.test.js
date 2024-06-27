3343507e488f1ccfc28a896a09792ad518a52282
Joao Cardoso <joao.cardoso@nn.nl>
2024-06-27 18:20:13 +0200

    Simplifying index

--- index.test.js
+++ index.test.js
@@ -2,13 +2,10 @@ const sum = require('./index')
 
 describe('sum', () => {
     test('should not return undefined for invalid input', () => {
-        const invalidResponse = undefined
-        expect(sum()).toBe(invalidResponse)
-        expect(sum(1, undefined)).toBe(invalidResponse)
-        expect(sum(undefined, 1)).toBe(invalidResponse)
+        expect(sum()).toBe()
     })
 
-    test('should return sum of two number', () => {
+    test('should return sum of two numbers', () => {
         expect(sum(1, 2)).toBe(3)
     })
 })
