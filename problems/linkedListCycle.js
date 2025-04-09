1e8913749a01adecae34318643f15916ef9c164c
Joao Cardoso <joao.cardoso@nn.nl>
2025-04-09 15:25:43 +0200

    linkedListCycle

---
+++ problems/linkedListCycle.js
@@ -0,0 +1,26 @@
+function ListNode(val, next) {
+    this.val = val
+    this.next = next
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let curr = head, subList = new Set()
+    while (curr) {
+        if (subList.has(curr.val)) {
+            return true
+        }
+        subList.add(curr.val)
+        curr = curr.next
+    }
+
+    return false
+};
+
+module.exports = {
+    ListNode,
+    hasCycle
+}
