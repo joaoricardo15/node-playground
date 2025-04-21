6ac4ae0ab739237e8a7194bc808e5f1b524d5ae1
Joao Cardoso <joao.cardoso@nn.nl>
2025-04-21 12:48:04 +0200

    elementsAroundThree

---
+++ problems/elementsAroundThree.js
@@ -0,0 +1,88 @@
+/**
+ * Given a three, return the elements around it in anticlockwise order.
+ * Elements around it are the nodes at the left and right of the three in each layer.
+ */
+
+/**
+ * Visual examples:
+ * 
+ *                 1
+ * 
+ * Answer: [1]
+ * -----------------------------------
+ * 
+ *                 1
+ *              /  |  \
+ *             2   3   4
+ * 
+ * Answer: [2, 1, 4]
+ * -----------------------------------
+ * 
+ *                 3
+ *            /    |    \
+ *          4      6     9
+ *        /   \   /    /   \
+ *       2     7 1    4    12
+ *     /   \        /   \
+ *    13   10      11   14
+ *                /  \
+ *               5    8
+ * 
+ * 
+ * 
+ * Answer: [5, 13, 2, 4, 3, 9, 12, 14, 8]
+ * */
+
+function GraphNode(val, children) {
+    this.val = val
+    this.children = children || []
+}
+
+/**
+ * @param {ListNode} head
+ * @return {Array}
+ */
+const elementsAroundThree = (head) => {
+    // Declare array for the eventual output
+    const elementsAround = [head.val]
+
+    // Declare array to handle nodes to be explored in each layer
+    let nodesToExplore = head.children
+    
+    // Run until there is no more nodes to be explored
+    while(nodesToExplore.length) {
+        // Declare empty aray to add nodes of current layer
+        const nextNodes = []
+
+        // Loop through nodes to be explored
+        for (let i=0; i<nodesToExplore.length; i++) {
+            // For each node
+            const node = nodesToExplore[i]
+
+            // Push its childen to be explored in the next run
+            nextNodes.push(...node.children)
+
+            // If node is the extreme right node
+            if (i === 0) {
+                // Add it to the start of output array
+                elementsAround.unshift(node.val)
+            }
+            // If node is the extreme left node
+            else if (i === nodesToExplore.length - 1) {
+                // Add it to the end of output array
+                elementsAround.push(node.val)
+            }
+        }
+
+        // Set the next nodes to explore as the concatenation
+        // of all children from all nodes in current layer
+        nodesToExplore = nextNodes
+    }
+
+    return elementsAround
+}
+
+module.exports = {
+    GraphNode,
+    elementsAroundThree
+}
---
+++ problems/tests/elementsAroundThree.test.js
@@ -0,0 +1,46 @@
+const { GraphNode, elementsAroundThree } = require('../elementsAroundThree')
+
+describe('elementsAroundThree', () => {
+    test('should return correct elements around three regarding visual example 1', () => {
+        const three = new GraphNode(1)
+        
+        expect(elementsAroundThree(three)).toStrictEqual([1])
+    })
+
+    test('should return correct elements around three regarding visual example 2', () => {
+        const three = new GraphNode(1, [
+            new GraphNode(2),
+            new GraphNode(3),
+            new GraphNode(4)
+        ])
+
+        expect(elementsAroundThree(three)).toStrictEqual([2, 1, 4])
+    })
+
+    test('should return correct elements around three regarding visual example 3', () => {
+        const three = new GraphNode(3, [
+            new GraphNode(4, [
+                new GraphNode(2, [
+                    new GraphNode(13),
+                    new GraphNode(10)
+                ]),
+                new GraphNode(7)
+            ]),
+            new GraphNode(6, [
+                new GraphNode(1)
+            ]),
+            new GraphNode(9, [
+                new GraphNode(4, [
+                    new GraphNode(11, [
+                        new GraphNode(5),
+                        new GraphNode(8)
+                    ]),
+                    new GraphNode(14)
+                ]),
+                new GraphNode(12)
+            ])
+        ])
+        
+        expect(elementsAroundThree(three)).toStrictEqual([5, 13, 2, 4, 3, 9, 12, 14, 8])
+    })
+})
