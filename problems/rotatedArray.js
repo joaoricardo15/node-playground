50ac8ff7b5c20486fa509b2b9850cfdd17add835
Joao Cardoso <joao.cardoso@nn.nl>
2025-06-27 14:20:36 +0200

    correcting commented algo

--- problems/rotatedArray.js
+++ problems/rotatedArray.js
@@ -1,12 +1,10 @@
-const test = [4, 5, 6, 7, 8, 9, 1, 2, 3];
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 const searchRotatedArray = (nums, target) => {
-  // return nums.indexOf(target)
+  return nums.indexOf(target)
 };
 
 module.exports = searchRotatedArray;
