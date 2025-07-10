1eb4ca345623b43316cc1981906f65278555d1b9
Joao Cardoso <joao.cardoso@nn.nl>
2025-07-10 17:42:56 +0200

    making sample test simpler

--- index.test.js
+++ index.test.js
@@ -1,10 +1,6 @@
 const sum = require('.')
 
 describe('sum', () => {
-  test('should not return undefined for invalid input', () => {
-    expect(sum()).toBe()
-  })
-
   test('should return sum of two numbers', () => {
     expect(sum(1, 2)).toBe(3)
   })
